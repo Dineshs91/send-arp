461f03abf521ca5154b3189a72ab5f7938a5aa43
dinesh <dineshpy07@gmail.com>
2017-04-04 17:01:32 +0530

    Add arp operation to cli options

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,29 @@
 use std::net::{Ipv4Addr, AddrParseError};
 use pnet::util::{MacAddr, ParseMacAddrErr};
+use pnet::packet::arp::{ArpOperation, ArpOperations};
 use clap::{Arg, App};
 
 use config::Config;
+
+const ABOUT: &'static str = "
+Send an arp packet.
+
+By default an arp request will be sent.
+";
 
 pub fn cli_main() -> Config {
     let matches = App::new("send-arp")
                           .version("0.1.0")
                           .author("Dineshs91 <dineshpy07@gmail.com>")
-                          .about("Send an arp reply packet")
+                          .about(ABOUT)
+                          .arg(Arg::with_name("interface")
+                              .short("i")
+                              .required(true)
+                              .takes_value(true)
+                              .help("Provide the interface to be used to send packets"))
+                          .arg(Arg::with_name("arp_reply")
+                              .long("reply")
+                              .help("Send an arp reply packet"))
                           .arg(Arg::with_name("source_ip")
                               .long("source-ip")
                               .required(true)
@@ -31,6 +46,8 @@ pub fn cli_main() -> Config {
                               .help("Set the target mac address"))
                           .get_matches();
 
+    let interface: String = matches.value_of("interface").unwrap().trim().to_string();
+
     let source_ip: Result<Ipv4Addr, AddrParseError> = matches.value_of("source_ip").unwrap()
                                                       .trim().parse();
     let source_mac: Result<MacAddr, ParseMacAddrErr> = matches.value_of("source_mac").unwrap()
@@ -40,11 +57,19 @@ pub fn cli_main() -> Config {
     let target_mac: Result<MacAddr, ParseMacAddrErr> = matches.value_of("target_mac").unwrap()
                                                        .trim().parse();
 
+    let mut arp_operation: ArpOperation = ArpOperations::Request;
+
+    if matches.is_present("arp_reply") {
+        arp_operation = ArpOperations::Reply;
+    }
+
     let config: Config = Config {
+        interface: interface,
         source_ip: source_ip.unwrap(),
         source_mac: source_mac.unwrap(),
         target_ip: target_ip.unwrap(),
-        target_mac: target_mac.unwrap()
+        target_mac: target_mac.unwrap(),
+        arp_operation: arp_operation
     };
 
     // return cli config.
--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,15 @@
 use std::net::Ipv4Addr;
 use pnet::util::MacAddr;
+use pnet::packet::arp::ArpOperation;
 
 
 /// Config for storing the cli options.
 #[derive(Debug)]
 pub struct Config {
+    pub interface: String,
     pub source_ip: Ipv4Addr,
     pub source_mac: MacAddr,
     pub target_ip: Ipv4Addr,
-    pub target_mac: MacAddr
+    pub target_mac: MacAddr,
+    pub arp_operation: ArpOperation
 }
